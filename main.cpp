2f32e6c1a970301b84ec04661db9e877780fdf58
miragecentury <victorien.vanroye@gmail.com>
2013-06-17 09:47:28 +0200

    - first

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zeus
+ *
+ * Created on 17 juin 2013, 09:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
